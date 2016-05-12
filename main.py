45504b8213432a158c3aa8109df1c75be456ba05
edward <edwardw@qq.com>
2016-05-12 16:54:10 +0800

    local

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # @Author: edward
 # @Date:   2016-05-12 10:19:03
 # @Last Modified by:   edward
-# @Last Modified time: 2016-05-12 11:00:04
+# @Last Modified time: 2016-05-12 16:34:37
 from tornado.web import (
     RequestHandler, Application, url, HTTPError,authenticated)
 from tornado.ioloop import IOLoop
 from tornado.options import define
 import tornado
+from tornado.escape import json_encode
 import os
-
+import swagger
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
 
@@ -27,20 +28,37 @@ def render_template(template_name, **context):
     # jinja_env.update_template_context(context)
     return jinja_env.get_template(template_name).render(context)
 
+
+
+        
 class HomeHandler(RequestHandler):
+    @swagger.operation(
+        nickname="home nickname"
+    )
     def get(self):
         # raise Exception(self.request.full_url()) 
         # http://localhost:8888/
         self.write("Hello,world")
 
-class ApiDocs(RequestHandler):
+    @swagger.operation(
+        nickname="home post"
+    )
+    def post(self):
+        self.write('test post')
+        
+class Docs(RequestHandler):
 	def get(self):
 		# self.write('test')
 		r = render_template('docs.html')
 		self.write(r)
+
+
 class ApiSpec(RequestHandler):
-	def get(self):
-		self.write(open('spec.json').read())
+    def get(self):
+        # self.write(open('spec.json').read())
+        json_spec = json_encode(swagger.get_api_spec())
+        self.write(json_spec)
+
 # tornado资源配置
 settings = {
     'template_path': os.path.join(os.path.dirname(__file__), 'static'),
@@ -50,11 +68,11 @@ settings = {
     'debug': True,
     'cookie_secret': 'abcdefg',
 }
-
-Application([
+handlers = [
     (r'/?', HomeHandler),
-    (r'/docs/?', ApiDocs),
-    (r'/api/spec.json?', ApiSpec),
-    ], default_host="0.0.0.0", **settings).listen(8888)
+    (r'/docs/?', Docs),
+    (r'/api/spec.json', ApiSpec),
+]
+Application(handlers, default_host="0.0.0.0", **settings).listen(8888)
 
 IOLoop.current().start()
---
+++ swagger.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# @Author: edward
+# @Date:   2016-05-12 14:11:21
+# @Last Modified by:   edward
+# @Last Modified time: 2016-05-12 16:40:10
+from functools import partial
+
+# api = swagger.docs(Api(app), apiVersion='0.1',
+#                    basePath='http://localhost:5000',
+#                    resourcePath='/',
+#                    produces=["application/json", "text/html"],
+#                    api_spec_url='/api/spec',
+#                    description='A Basic API')
+
+class _APIs:
+    def __init__(self):
+        self.apis = []
+        self.make_docs()
+    def add_api(self, method, **kw):
+        self.apis.append(self.pre_api(method, **kw))
+
+    def operation(self, **kw):
+        return partial(self.add_api, **kw)
+
+    def pre_api(self, fn, **kw):
+         d = dict(
+                method=fn.__name__,
+            )
+         d.update(kw)
+         return d
+
+    def get_spec(self):
+        return self.apis 
+
+    def make_docs(self, apiVersion='1.0', swaggerVersion='1.2',
+             basePath='http://localhost:9999',
+             resourcePath='/',
+             produces=["application/json"],
+             api_spec_url='/api/spec',
+             description='Auto generated API docs by swagger'):
+        self.API_VERSION = apiVersion
+        self.SWAGGER_VERSION = swaggerVersion
+        self.BASEPATH = basePath
+        self.RESOURCE_PATH = resourcePath
+        self.PRODUCES = produces
+        self.API_SPEC_URL = api_spec_url
+        self.DESCRIPTION = description
+
+
+apis = _APIs()
+operation = apis.operation
+docs = apis.make_docs
+get_api_spec = apis.get_spec
+
+
+def main():
+    class Handler:
+        @operation(
+            nickname="get something from api"
+        )
+        def get(self):
+            """this is get notes """
+            print 'get'
+
+        def post(self):
+            print 'post'
+
+    for i in apis.apis:
+        print i
+
+
+if __name__ == '__main__':
+    main()
+
+
